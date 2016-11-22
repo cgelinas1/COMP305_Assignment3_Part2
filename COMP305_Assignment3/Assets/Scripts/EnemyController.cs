4bd4dcb6c8c005dd35bd5c575aeb914eb8fbc431
cgelinas1 <cgelinas@my.centennialcollege.ca>
2016-11-22 16:37:54 -0500

    Added Enemy and basic EnemyController
    added enemy object, spotlight, and basic enemy movement

---
+++ COMP305_Assignment3/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    
+    public NavMeshAgent Agent;
+
+    //private instance variables
+    private Transform Player;
+
+
+	// Use this for initialization
+	void Start () {
+        this.Player = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.Agent.SetDestination(this.Player.position);
+	}
+}

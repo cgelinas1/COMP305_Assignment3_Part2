62d75a06e6b77070e72cc8d3b51b55125411aeea
cgelinas1 <cgelinas@my.centennialcollege.ca>
2016-11-23 19:20:20 -0500

    edited enemy ai

--- COMP305_Assignment3/Assets/Scripts/EnemyController.cs
+++ COMP305_Assignment3/Assets/Scripts/EnemyController.cs
@@ -1,22 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyController : MonoBehaviour {
+public class EnemyController : MonoBehaviour
+{
+    //Christopher Gelinas 300844877 COMP305 Assignment 3 Part 2
 
-    
     public NavMeshAgent Agent;
-
+    public Transform SightStart;
+    public Transform SightEnd;
+    
     //private instance variables
     private Transform Player;
+    public BoxCollider _boxCollider;
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         this.Player = GameObject.FindWithTag("Player").transform;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        this.Agent.SetDestination(this.Player.position);
-	}
+        this._boxCollider = GetComponent<BoxCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+
+
+
+
+    }
+
+    
+    private void OnCollisionEnter(Collision BoxCollider)
+    {
+        
+
+        if (BoxCollider.gameObject.CompareTag("Player"))
+        {
+           
+
+
+                this.Agent.SetDestination(this.Player.position);
+            
+
+        }
+    }
 }
